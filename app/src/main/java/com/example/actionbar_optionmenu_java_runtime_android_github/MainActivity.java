a0037aa2a873e279571cac191e54f547e9a9ecd7
Aman Sharma <amansharma.11@hotmail.com>
2019-07-19 04:09:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/actionbar_optionmenu_java_runtime_android_github/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.actionbar_optionmenu_java_runtime_android_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
